86a6cb020c29d8ddd54c19d7082fa8e670953ee2
Deepak782001 <deepaksharma782001@gmail.com>
2022-09-02 22:41:40 +0530

    MongoDB server

--- Day-10-MangoDB/server.js
+++ Day-10-MangoDB/server.js
@@ -4,13 +4,23 @@ const app = express();
 const connectToDB = require("./DB/index.js");
 const Usermodel = require("./models/users.model.js");
 
-
 const PORT = 8000;
 
 connectToDB();
+// using this we can send and view json objects
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("It is working");
+});
+
+app.post("/adduser", (request, response) => {
+  //console.log(request.body);
+  
+  // destructureing of data
+  const { names, phone } = request.body;
+  console.log(names, phone)
+  response.send("please check ");
 });
 
 console.log(Usermodel);
